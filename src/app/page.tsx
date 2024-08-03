6cc73493d6b558d61d400936360bbac6c68d8978
3jasoon3 <33jasooon33@gmail.com>
2024-08-03 10:17:23 +0300

    New graph

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import styles from "@/styles/HomePage.module.scss";
 import Image from "next/image";
 import Link from "next/link";
@@ -6,12 +8,7 @@ import github from "/public/svg/github.svg";
 import telegram from "/public/svg/telegram.svg";
 import linkedin from "/public/svg/linkedin.svg";
 import twitter from "/public/svg/twitter.svg";
-
-import type { Metadata } from "next";
-
-export const metadata: Metadata = {
-  title: "Egor Chernobrovkin"
-};
+import "./globals.scss";
 
 export default function Home() {
   return (
@@ -21,7 +18,7 @@ export default function Home() {
           <div className={styles.personalInfoText}>
             <div className={styles.personalInfoTextMsg}>Hi! 👋</div>
             <div className={styles.personalInfoTextBio}>
-              <b style={{ color: "black" }}>I am Egor,</b> a NLP Engineer
+              <b style={{ color: "black" }}>I am Egor,</b> an NLP Engineer
               studying at Innopolis University.
             </div>
             <div className={styles.contactsContainer}>
@@ -53,6 +50,41 @@ export default function Home() {
         <div className={styles.separatorContainer}>
           <div className={styles.separator}></div>
         </div>
+      </div>
+      <div className={styles.timeLineContainer}>
+        <ol className="relative border-s border-gray-200 dark:border-gray-700">
+          <li className="mb-10 ms-4">
+            
+            <div className="absolute w-3 h-3 bg-gray-200 rounded-full mt-1.5 -start-1.5 border border-white dark:border-gray-900 dark:bg-gray-700"></div>
+            <Image src={twitter} width={32} height={32} alt="x" />
+            <time className="mb-1 text-sm font-normal leading-none text-gray-400 dark:text-gray-500">
+              April 2024
+            </time>
+            <h3 className="text-lg font-semibold text-gray-900 dark:text-white">
+              Advanced Engineering School Trainee
+            </h3>
+            
+            <p className="mb-4 text-base font-normal text-gray-500 dark:text-gray-400">
+              Get access to over 20+ pages including a dashboard layout, charts,
+              kanban board, calendar, and pre-order E-commerce & Marketing
+              pages.
+            </p>
+          </li>
+          <li className="mb-10 ms-4">
+            <div className="absolute w-3 h-3 bg-gray-200 rounded-full mt-1.5 -start-1.5 border border-white dark:border-gray-900 dark:bg-gray-700"></div>
+            <time className="mb-1 text-sm font-normal leading-none text-gray-400 dark:text-gray-500">
+              August 2024
+            </time>
+            <h3 className="text-lg font-semibold text-gray-900 dark:text-white">
+              Innopolis University BSc enrollment
+            </h3>
+            <p className="text-base font-normal text-gray-500 dark:text-gray-400">
+              All of the pages and components are first designed in Figma and we
+              keep a parity between the two versions even as we update the
+              project.
+            </p>
+          </li>
+        </ol>
       </div>
     </div>
   );
